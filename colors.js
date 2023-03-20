2d66c5fc721989d1d58722a796c72f9b26d92914
Nicolás Antonelli <niconelli2@gmail.com>
2023-03-20 20:41:51 -0300

    Colors.js: add modular color palette

---
+++ colors.js
@@ -0,0 +1,10 @@
+// Color palette
+const BlackMain = '#333'
+const BlackSecond = '#222'
+const BlackThird = '#444'
+const Blue = '#0052CC'
+const GrayBackground = '#CCC'
+const White = '#FFF'
+
+const Colors = { BlackMain, BlackSecond, BlackThird, Blue, GrayBackground, White }
+export default Colors
