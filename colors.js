e6e73d26241e4e86b779d81aefb78f0f3bcd9ad8
Nicolás Antonelli <niconelli2@gmail.com>
2023-03-20 21:23:22 -0300

    Colors: improve palette

--- colors.js
+++ colors.js
@@ -1,10 +1,24 @@
 // Color palette
-const BlackMain = '#333'
-const BlackSecond = '#222'
-const BlackThird = '#444'
-const Blue = '#0052CC'
-const GrayBackground = '#CCC'
-const White = '#FFF'
+const Colors = {
+    BlackMain: '#333',
+    BlackSecond: '#222',
+    BlackThird: '#444',
+    Blue: '#0052CC',
+    Cyan: '#429eff',
+    GrayBackground: '#CCC',
+    GrayDisabled: '#aaa',
+    GrayHover: '#555',
+    White: '#FFF',
 
-const Colors = { BlackMain, BlackSecond, BlackThird, Blue, GrayBackground, White }
+    // Shadows
+    Shadow13: rgba(150, 150, 150, 0.13),
+    Shadow25: rgba(150, 150, 150, 0.25),
+
+    // Others
+    BackgroundSelected: '#353a44',
+    BackgroundSelectedHover: '#484f5d',
+    TextSelected: '#9494ff',
+    TextSubtle: '#d2d2d2',
+}
+
 export default Colors
